851c6ad402a856d50109bc09accd15e45f70f4f7
Pei-Yun Tseng <peitseng@MacBook-Pro-22.home>
2021-11-23 09:21:29 -0600

    fixed items and history page

--- Stronger/public/javascripts/checkout.js
+++ Stronger/public/javascripts/checkout.js
@@ -1,5 +1,38 @@
 $(document).ready(function () {    
 
+    $.ajax({
+        method: 'GET',
+        url:'/api/shoppingcart/' + user_username,
+        success: function(cart){
+            console.log(cart);
+            console.log(cart.items);
+            console.log(cart.items[0].item_image);
+            $.each(cart.items, function(i, item){
+                console.log(item.item_image);
+                var order_card = `<div class="card mb-5">
+                <img src="/images/Equipment/` + item.item_image + `" class="card-img-top" />
+                <div class="card-body">
+                  <h5 class="card-title">` + item.item_name + `</h5>
+                  <p class="card-text">
+                    <div id="price">` + item.item_price +  `</div>
+                    <div id="quantity"> Qty:` + item.item_quantity +  `</div>
+                  </p>
+                </div>
+              </div>`
+              $("#items_card").append(order_card)
+            })
+        },
+
+            // var item_image = cart.item_image,
+            // var item_name = cart.item_name,
+            // var item_price = cart.item_price,
+            // var item_quantity = cart.item_quantity,
+
+        error: function(){
+            alert("Error loading orders");
+        },
+    });
+
     $("#btn_order").click('submit', function () {
 
         //Shipping variables
@@ -39,6 +72,16 @@ $(document).ready(function () {
 
         console.log(customer);
 
+        var items = {}
+
+        var order = {
+            customer_id: user._id,
+            customer_username: user_username,
+            order: [items],
+            total_price: tot_price,
+            date: current_date
+        }
+
         //PUT customer object
         // $.ajax({
         //     method: 'PUT',
@@ -70,4 +113,8 @@ $(document).ready(function () {
         //     },
         // });
         // }
+
+        function calculateTotal(item_price, quantity){
+            return item_price * quantity;
+        }
 });
--- Stronger/public/javascripts/orderitems.js
+++ Stronger/public/javascripts/orderitems.js
@@ -8,13 +8,25 @@ $(document).ready(function(){
         success: function(order){
             $("h2").append(order._id);
             $("#date").append(order.date);
-            console.log(order.order);
-            console.log(order.order[0]);
+            // console.log(order.order);
+            // console.log(order.order[0]);
             $.each(order.order, function(i, o){
-                $("#equipment_image").attr("src", order.order[i].equipment);
-                $(".card-title").append(order.order[i].equipment_name);
-                $("#price").append("$" + order.order[i].equipment_price);
-                $("#quantity").append(order.order[i].quantity);
+                console.log(o);
+                var order_card = `<div class="card mb-5">
+                <img src="/images/equipment/` + o.equipment_image + `" class="card-img-top" />
+                <div class="card-body">
+                  <h5 class="card-title">` + o.equipment_name + `</h5>
+                  <p class="card-text">
+                    <div id="price">` + o.equipment_price +  `</div>
+                    <div id="quantity"> Qty:` + o.quantity +  `</div>
+                  </p>
+                </div>
+              </div>`
+              $("#item_card").append(order_card);
+                // $("#equipment_image").attr("src", order.order[i].equipment);
+                // $(".card-title").append(order.order[i].equipment_name);
+                // $("#price").append("$" + order.order[i].equipment_price);
+                // $("#quantity").append(order.order[i].quantity);
             });
             $("#total").append("$" + order.total_price);
     },
