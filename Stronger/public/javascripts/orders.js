d1043c09b1f31ca6c9ea618708cd2ce863ed7777
Pei-Yun Tseng <peitseng@MacBook-Pro-22.home>
2021-11-25 20:06:51 -0600

    fixed address display

--- Stronger/public/javascripts/orders.js
+++ Stronger/public/javascripts/orders.js
@@ -1,31 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     $.ajax({
         method: 'GET',
-        url:'/orders?customerid=' + user_id,
-        success: function(orders){
-            $.each(orders, function(i, order){
+        url: '/orders?customerid=' + user_id,
+        success: function (orders) {
+            $.each(orders, function (i, order) {
                 console.log(total_price);
-                $("#id").append(`<a href="/orderitems?id=` + order._id + `">` + order._id +  `</a><br>`);
+                $("#id").append(`<a href="/orderitems?id=` + order._id + `">` + order._id + `</a><br>`);
                 $("#date").append(order.date + '<br>');
                 $("#total_price").append(`$` + order.total_price + '<br>');
-        });
-    },
-        error: function(){
+            });
+        },
+        error: function () {
             alert("Error loading orders");
         },
     });
 
     $.ajax({
         method: 'GET',
-        url:'/customers/' + user_username,
-        success: function(user){
-            $("#address").append(`Address: <br>` + user.billing_address.bill_address + `<br>` + 
-            user.billing_address.bill_apt + `<br>` + user.billing_address.bill_city + `, `
-            + user.billing_address.bill_state + ` ` + user.billing_address.bill_zip +
-            `<br>` + user.billing_address.bill_country);
-    },
-        error: function(){
+        url: '/customers/' + user_username,
+        success: function (user) {
+            if (user.billing_address != "" && user.billing_address.bill_apt != "") {
+                $("#address").append(`Address: <br>` + user.billing_address.bill_address + `<br>` +
+                    user.billing_address.bill_apt + `<br>` + user.billing_address.bill_city + `, `
+                    + user.billing_address.bill_state + ` ` + user.billing_address.bill_zip +
+                    `<br>` + user.billing_address.bill_country);
+            }
+            else if (user.billing_address != ""){
+                $("#address").append(`Address: <br>` + user.billing_address.bill_address + `<br>` + user.billing_address.bill_city + `, `
+                + user.billing_address.bill_state + ` ` + user.billing_address.bill_zip +
+                `<br>` + user.billing_address.bill_country);
+            }
+        },
+        error: function () {
             alert("Error finding customer");
         },
     });
