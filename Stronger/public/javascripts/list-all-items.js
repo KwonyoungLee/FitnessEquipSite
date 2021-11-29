5d6ad49431b80b30f8cb324c72e7ad5c101ea222
Pei-Yun Tseng <peiyun.t@gmail.com>
2021-11-29 13:06:33 -0600

    added dollar signs to list-all-items

--- Stronger/public/javascripts/list-all-items.js
+++ Stronger/public/javascripts/list-all-items.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
                 $("#items_list").after(`<div class=row>
                 <div class=col-4>`+ item.item_name +`</div>
-                <div class=col-1>` + item.price + `</div>
+                <div class=col-1>$` + item.price + `</div>
                 <div class=col-2>`+ item.category +`</div>
                 <div class=col-3>`+ item.image +`</div>
                 <div class=col-1>`+ item.quantity_in_stock +`</div>
